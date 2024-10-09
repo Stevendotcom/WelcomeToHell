036455769682ddce0b1ec80dd6b496c356fcb1ff
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-10-09 10:25:02 -0300

    add start logic

---
+++ src/engine/StartUp.h
@@ -0,0 +1,3 @@
+﻿namespace StartUp {
+void Start();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-﻿#include "raylib.h"
+﻿#include "engine/StartUp.h"
 #include "engine/SceneManager.h"
 enum class ScreenSize {
   Height = 1024,
   Width = 768,
-}
+};
 
 int main(){
-  
+  SceneManager::ChangeScene();
 }
