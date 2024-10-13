f92852992afdeeaadbf535dbe3525e90091b8740
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-10-13 17:45:55 -0300

    add circle circle collision
    Signed-off-by: Alex Gallardo <gallardonicolas.8100@outlook.com>

---
+++ src/engine/Collisions.cpp
@@ -0,0 +1,17 @@
+﻿#include "Collisions.h"
+
+
+
+bool Collisions::IsCircleCircle(const Vector2& APosition,
+                                float ARadius,
+                                const Vector2& BPosition,
+                                float BRadius) {
+  const float k_Dist = (APosition.x - BPosition.x) * (APosition.x - BPosition.x)
+                       + (APosition.y - BPosition.y) * (
+                         APosition.y - BPosition.y);
+  const float k_Radii = (ARadius + BRadius) * (ARadius + BRadius);
+
+  // Sum(Radius)^2 >= Dist^2
+  // saves the necessity of a sqrt
+  return k_Radii >= k_Dist;
+}
---
+++ src/engine/Collisions.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <raylib.h>
+
+namespace Collisions {
+/**
+ *
+ */
+bool IsCircleCircle(const Vector2& APosition, float ARadius, const Vector2& BPosition, float BRadius);
+  
+};
