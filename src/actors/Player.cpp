4c6a3b8c72c303724c40968a1cf41ee0f73ad8a3
Alex Gallardo <gallardonicolas.8100@outlook.com>
2025-03-10 20:39:27 -0300

    add defAccel to be able to return once Boost Timer has beeen reached
    Signed-off-by: Alex Gallardo <gallardonicolas.8100@outlook.com>

--- src/actors/Player.cpp
+++ src/actors/Player.cpp
@@ -14,20 +14,26 @@ namespace {
 
 constexpr float k_MaxSpeed = 500.0F;
 constexpr float k_DefRadius = 40.0F;
-constexpr int k_DefHearts = 3;
 constexpr float k_DefAccel = 1000.0F;
+constexpr int k_DefHearts = 3;
 
-} // namespace
+}
+
 
 void Player::Initialize(PlayerType& Player) {
   Player = {k_DefAccel,
             k_DefRadius,
+            k_DefAccel,
             k_DefHearts,
             0,
-            {g_ScreenWidth / 2.0F, g_ScreenHeight / 2.0F},
-            {0.0F, 0.0F},
-            {0.0F, -1.0F},
+            {g_ScreenWidth / 2.0F,
+             g_ScreenHeight / 2.0F},
+            {0.0F,
+             0.0F},
+            {0.0F,
+             -1.0F},
             GetTexture(ResManager::Resources::PlayerSprite),
+            false,
             false};
 }
 
@@ -84,13 +90,15 @@ void Player::UpdateDuplicate(PlayerType& Player,
                              bool& DuplicatedVisible) {
   Duplicated = {Player.f_Acceleration,
                 Player.f_Radius,
+                Player.f_Acceleration,
                 Player.f_Hearts,
                 Player.f_Score,
                 Duplicated.f_Position,
                 Player.f_Speed,
                 Player.f_Direction,
                 Player.f_Sprite,
-                Player.f_IsInvencible};
+                Player.f_IsInvencible,
+                Player.f_IsPowerBoostActive};
 
   Update(Duplicated, GetFrameTime(), true);
 
@@ -155,6 +163,7 @@ void Player::Update(PlayerType& Player,
 
     Player.f_Direction = Normalize(k_Temp, k_Mag);
   }
+
   Player.f_Position = {Player.f_Position.x + Player.f_Speed.x * Delta,
                        Player.f_Position.y + Player.f_Speed.y * Delta};
 }
@@ -164,6 +173,7 @@ void Player::Update(PlayerType& Player,
 void Player::Draw(const PlayerType& Player) {
   constexpr float k_RotCorrection = 90.0F;
   constexpr float k_Scale = 2.0F;
+
   const Rectangle Source = {0,
                             0,
                             static_cast<float>(Player.f_Sprite.width),
@@ -174,8 +184,12 @@ void Player::Draw(const PlayerType& Player) {
                           Player.f_Radius * k_Scale * 2.0F,
                           Player.f_Radius * k_Scale * 2.0F};
 
-  DrawTexturePro(Player.f_Sprite, Source, Dest,
-                 {Player.f_Radius * k_Scale, Player.f_Radius * k_Scale},
+  DrawTexturePro(Player.f_Sprite, Source, Dest, {Player.f_Radius * k_Scale,
+                                                 Player.f_Radius * k_Scale},
                  k_RotCorrection - GetRotation(Player.f_Direction),
                  Player.f_IsInvencible ? RED : WHITE);
+
+#ifdef _DEBUG
+  DrawCircleLinesV(Player.f_Position, Player.f_Radius,WHITE);
+#endif
 }
