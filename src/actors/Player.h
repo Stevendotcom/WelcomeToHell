a92372ff26dfd18555c5d477c96630b9b6e5def4
Alex Gallardo <gallardonicolas.8100@outlook.com>
2025-03-23 17:12:22 -0300

    add fire sprites to player
    Signed-off-by: Alex Gallardo <gallardonicolas.8100@outlook.com>

--- src/actors/Player.h
+++ src/actors/Player.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
+#include "engine/Animations.h"
 #include "engine/Collisions.h"
-#include "engine/Math.h"
 
 namespace Player {
 
@@ -8,14 +8,20 @@ struct PlayerType {
   float f_Acceleration;
   float f_Radius;
   float f_DefAccel;
+  float f_FrameTime;
   int f_Hearts;
   int f_Score;
+  int f_FrameIndex;
   Vector2 f_Position;
   Vector2 f_Speed;
   Vector2 f_Direction;
+  Vector2 f_JetpackEndPosition;
   Texture f_Sprite;
+  Texture f_SpriteFire;
+  Animations::Frame f_Frame;
   bool f_IsInvencible;
   bool f_IsPowerBoostActive;
+  bool f_IsAccelerating;
 };
 
 
